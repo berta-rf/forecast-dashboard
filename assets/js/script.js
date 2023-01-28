875620f10f765504929dd25f4b48d9f7f87bbe4d
Berta Romano Fernandez <bertaromanofernandez@gmail.com>
2023-01-28 11:36:34 +0000

    Use fetch() for api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
 $( document ).ready(function() {
-
+    
     //Display the current date
-    const dateTime = luxon.DateTime;
-    const today = dateTime.now().setZone("system");
-    let currentDate = today.toLocaleString(dateTime.DATE_SHORT);
-    console.log(today);
-
-// When a user searches for a city they are presented with current and future weather conditions for that city
+    const DateTime = luxon.DateTime;
+    const today = DateTime.now().setZone("system");
+    let currentDate = today.toLocaleString(DateTime.DATE_SHORT);
+    
+    // When a user searches for a city they are presented with current and future weather conditions for that city
     const key = "dcbf10813d2cfd90e585dbc6c1042563";
-    let lat;
-    let lon;
+    
     
     // Adds a click event to Submit button to search city
     $('#search-button').on('click', function(e) {
@@ -17,135 +15,156 @@ $( document ).ready(function() {
         let cityInput = $('#search-input').val();
         // Clear input text
         $('#search-input').val("");
-
-//DRoPDOWN SAVED FOR LATER ------> When name of city is entered in search bar, I get up to 5 city options in dropdown
-        let cityQueryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${key}`;
-
+        
+        //DRoPDOWN SAVED FOR LATER ------> When name of city is entered in search bar, I get up to 5 city options in dropdown
+        // let cityQueryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${key}`;
+        
         e.preventDefault();
+                
+        let currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}&units=metric`;
         
-        $.ajax({
-            url: cityQueryURL,
-            method: "GET"
-        }).then(function(response) {
+        fetch(currentWeatherURL)
+            .then((response) => response.json())
+            .then((data) => {
 
-            //console.log(JSON.stringify(response));
-
-            let currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}&units=metric`;
+            // Converts wind speed in m/s to km/h  
+            let wind = parseInt(data.wind.speed)*3.6;
             
-            let forecastQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${response[0].lat}&lon=${response[0].lon}&appid=${key}&units=metric`;
-
+            // Create link for the weather icon source (img src)
+            let iconSrc = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
             
-            $.ajax({
-                url: currentWeatherURL,
-                method: "GET"
-            }).then(function(response) {
+            // Current weather by city shows city name, date, weather, temperature, humidity and wind speed (html template)
+            const currentWeather = $(
+                `
+                <h2 class="fw-bold">${data.name} ${currentDate}</h2>
+                <img style="width:70px" alt='weather icon' src="${iconSrc}"/>
+                <p>Temp: ${data.main.temp} ℃</p>
+                <p>Wind: ${wind} KM/H</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+                </div>
+                `
+            );
+                
+            $('#today').html(currentWeather);
+            
+            // 5-day-forecast URL with latitud and longitude from currentWeather ajax call (metric system and limit of 5 timestamps)
+            let forecastQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&units=metric&appid=${key}`;
+            
+            
+            fetch(forecastQueryURL)
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                          
+    
 
-                // Converts wind speed in m/s to km/h  
-                let wind = parseInt(response.wind.speed)*3.6;
-                console.log(wind)
+                // for (i = 0; i < response.list.length ; i++) {
 
-                // Create URL for the weather icon source (img src)
-                let iconSrc = `http://openweathermap.org/img/w/${response.weather[0].icon}.png`
+                //     let forecastDay = response.list[i];
+                //     // Converts wind speed in m/s to km/h 
+                //     wind = parseInt(forecastDay.wind.speed)*3.6;
 
-                // Current weather by city shows city name, date, weather, temperature, humidity and wind speed
-                const currentWeather = $(
-                    `
-                    <h2 class="fw-bold">${response.name} ${currentDate}</h2>
-                    <img style="width:70px" alt='weather icon' src="${iconSrc}"/>
-                    <p>Temp: ${response.main.temp} ℃</p>
-                    <p>Wind: ${wind} KM/H</p>
-                    <p>Humidity: ${response.main.humidity}%</p>
-                    </div>
-                    `
-                );
+                //     const luxonDate = DateTime.fromSQL(forecastDay.dt_txt).toLocaleString(DateTime.DATE_SHORT);
 
-                    // currentWeather.attr("data-date",LUXON);
-                    $('#today').html(currentWeather);
+                //     // let forecastDate;
+
+                //     // JSON.stringify(forecastDate)
+
+                //     wind = parseInt(forecastDay.wind.speed)*3.6;
+                //     iconSrc = `http://openweathermap.org/img/w/${forecastDay.weather[0].icon}.png`
+
+                //     // 5-day forecast section displays: date, weather icon, temperature and humidity
+                //     // Card component template for the 5 cards for 5-day-forecast
+                    // const forecastCard = $(
+                    //     `
+                    //     <div class="card m-2">
+                    //     <h4 class="card-header">${luxonDate}</h4>
+                    //     <img style="width:70px" alt='weather icon' src="${iconSrc}"/>
+                    //     <p>Min Temp: ${forecastDay.main.temp} ℃</p>
+                    //     <p>Min Temp: ${forecastDay.main.temp} ℃</p>
+                    //     <p>Max Temp: ${forecastDay.main.temp} ℃</p>
+                    //     <p>Wind: ${wind} KM/H</p>
+                    //     <p>Humidity: ${forecastDay.main.humidity}%</p>
+                    //     </div>
+                    //     `
+                    // );
+                            
+                //     $('.forecast-cards').append(forecastCard);
                     
+                // }
                 
-            });   
 
-                // Searched cities are added to the search history (localStorage).
-                
-                // let forecastQueryURL = `api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${key}&units=metric`;
+            });
                 
                 
-        });        
-        
+        });   
             
+                // Searched cities are added to the search history (localStorage).                
+                
+                     
+                
     });
-        
-        
-        
-        // 5-day forecast section displays: date, weather icon, temperature and humidity
-
-
-        // //    const forecastCard = $(
-        //     `
-        //      <div class="card" style="width: 18rem;">
-        // //         <h4 class="card-header"></h4>
-        // //          <p>Temp: ${response.main.temp} ℃</p>
-        // //          <p>Wind: ${wind} KM/H</p>
-        // //          <p>Humidity: ${response.main.humidity}%</p>
-        // //     </div>
-        //     `
-        //     );
-        
-        // When a user clicks on a city in the search history, current and future conditions for that city are shown again
-        
-         // Function for displaying buttons for each city in search history
-
+            
+            
+            
+            
+            // When a user clicks on a city in the search history, current and future conditions for that city are shown again
+            
+            // Function for displaying buttons for each city in search history
+            
             //  function renderButtons() {
             
             //     // loop through array
             //     // create a button for each + cityName + data-date attribute
-
+            
             //     for (let city of cities) {
-                
+            
             //       let cityName = $(RESPONSE.NAME)
             //       const cityBtn = $(`<button><li>${cityName}}</li></button>`);
             //       cityBtn.attr("data-date", LUXON);
             //       $('#history').prepend(cityBtn);
-                
+            
             //     }
             //   }
-                    
-   
-        
+            
+    $('#search-input').val('Paris');
+    $('#search-button').click()
+            
+            
 });
-
-                
-// event delegation because movie buttons are not created yet so we target the parent div buttons
-// $("#history").on("click", "button" , displayCurrenWeather)
-
-// function displayCurrentWeather(e) {
-  
-//   e.target.preventDefault;
-
-//   let date = $(e.target).attr('data-date');
-//   // Here we construct our URL
-//   var queryURL = "https://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
-  
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-    
-//     // let movieInfo = JSON.stringify(response);
-
-//     let movieInfo = $(
-//       `
-//       <div>
-//       <h2>${response.Title}</h2>
-//       <img src="${response.Poster}" alt="${response.Title} poster" width="500" height="600">
-//       <p>Release date: ${response.Released}</p>
-//       <p>Plot: ${response.Plot}</p>
-//     </div>
-//       `
-//     );
-    
-//     $('#movies-info').prepend(movieInfo);
-
-//   });
-// }
+        
+        
+        // event delegation because movie buttons are not created yet so we target the parent div buttons
+        // $("#history").on("click", "button" , displayCurrenWeather)
+        
+        // function displayCurrentWeather(e) {
+        
+        //   e.target.preventDefault;
+        
+        //   let date = $(e.target).attr('data-date');
+        //   // Here we construct our URL
+        //   var queryURL = "https://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
+        
+        //   $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        //   }).then(function(response) {
+        
+        //     // let movieInfo = JSON.stringify(response);
+        
+        //     let movieInfo = $(
+        //       `
+        //       <div>
+        //       <h2>${response.Title}</h2>
+        //       <img src="${response.Poster}" alt="${response.Title} poster" width="500" height="600">
+        //       <p>Release date: ${response.Released}</p>
+        //       <p>Plot: ${response.Plot}</p>
+        //     </div>
+        //       `
+        //     );
+        
+        //     $('#movies-info').prepend(movieInfo);
+        
+        //   });
+        // }
         
