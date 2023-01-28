fbac5f6e482c68a168f0f473d12bda6e006d7cc6
Berta Romano Fernandez <bertaromanofernandez@gmail.com>
2023-01-28 17:51:24 +0000

    Get 5-day forecast data with min and max temps and day-time icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,92 +20,119 @@ $( document ).ready(function() {
         // let cityQueryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${key}`;
         
         e.preventDefault();
-                
+        
         let currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}&units=metric`;
         
+        // request current weather data
         fetch(currentWeatherURL)
-            .then((response) => response.json())
-            .then((data) => {
-
-            // Converts wind speed in m/s to km/h  
-            let wind = parseInt(data.wind.speed)*3.6;
+        .then((response) => response.json())
+        .then((data) => {
             
-            // Create link for the weather icon source (img src)
-            let iconSrc = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+                // Converts wind speed in m/s to km/h  
+                let wind = parseInt(data.wind.speed)*3.6;
+                
+                // Create link for the weather icon source (img src)
+                let iconSrc = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+                let temp = Math.round(data.main.temp);
+                let feelTemp = Math.round(data.main.feels_like);
             
-            // Current weather by city shows city name, date, weather, temperature, humidity and wind speed (html template)
-            const currentWeather = $(
+                // Current weather by city shows city name, date, weather, temperature, humidity and wind speed (html template)
+                const currentWeather = $(
                 `
                 <h2 class="fw-bold">${data.name} ${currentDate}</h2>
                 <img style="width:70px" alt='weather icon' src="${iconSrc}"/>
-                <p>Temp: ${data.main.temp} ℃</p>
+                <p>Temp: ${temp} ℃</p>
+                <p>Feels like: ${feelTemp} ℃</p>
                 <p>Wind: ${wind} KM/H</p>
                 <p>Humidity: ${data.main.humidity}%</p>
                 </div>
                 `
-            );
+                );
                 
-            $('#today').html(currentWeather);
-            
-            // 5-day-forecast URL with latitud and longitude from currentWeather ajax call (metric system and limit of 5 timestamps)
-            let forecastQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&units=metric&appid=${key}`;
-            
-            
-            fetch(forecastQueryURL)
+                $('#today').html(currentWeather);
+                
+                // 5-day-forecast URL with latitud and longitude from currentWeather ajax call (metric system and limit of 5 timestamps)
+                let forecastQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&units=metric&appid=${key}`;
+                
+                // request 5 day forecast data
+                fetch(forecastQueryURL)
                 .then((response) => response.json())
                 .then((data) => {
-                    console.log(data);
-                          
-    
-
-                // for (i = 0; i < response.list.length ; i++) {
-
-                //     let forecastDay = response.list[i];
-                //     // Converts wind speed in m/s to km/h 
-                //     wind = parseInt(forecastDay.wind.speed)*3.6;
-
-                //     const luxonDate = DateTime.fromSQL(forecastDay.dt_txt).toLocaleString(DateTime.DATE_SHORT);
-
-                //     // let forecastDate;
-
-                //     // JSON.stringify(forecastDate)
-
-                //     wind = parseInt(forecastDay.wind.speed)*3.6;
-                //     iconSrc = `http://openweathermap.org/img/w/${forecastDay.weather[0].icon}.png`
-
-                //     // 5-day forecast section displays: date, weather icon, temperature and humidity
-                //     // Card component template for the 5 cards for 5-day-forecast
-                    // const forecastCard = $(
-                    //     `
-                    //     <div class="card m-2">
-                    //     <h4 class="card-header">${luxonDate}</h4>
-                    //     <img style="width:70px" alt='weather icon' src="${iconSrc}"/>
-                    //     <p>Min Temp: ${forecastDay.main.temp} ℃</p>
-                    //     <p>Min Temp: ${forecastDay.main.temp} ℃</p>
-                    //     <p>Max Temp: ${forecastDay.main.temp} ℃</p>
-                    //     <p>Wind: ${wind} KM/H</p>
-                    //     <p>Humidity: ${forecastDay.main.humidity}%</p>
-                    //     </div>
-                    //     `
-                    // );
-                            
-                //     $('.forecast-cards').append(forecastCard);
                     
-                // }
-                
+                    const fiveDayForecast = {};  
+                    
+                    for(let forecastDay of data.list) {
+                        
+                        // Separate date from time in dt_txt string (split returns 2 arrays, one for date one for time)
+                        const date = forecastDay.dt_txt.split(" ")[0];
+                        const time = forecastDay.dt_txt.split(" ")[1];
+                        
+                        if (!fiveDayForecast[date]) {
+                            fiveDayForecast[date] = {
+                                temp_min: forecastDay.main.temp_min,
+                                temp_max: forecastDay.main.temp_max,
+                                wind: forecastDay.wind.speed,
+                                humidity: forecastDay.main.humidity,
+                                icon: forecastDay.weather[0].icon,
+                                description: forecastDay.weather[0].description,
+                            }  
+                            
+                        } else {
+                        // Iterates through each 3h min and max temps and returns the lowest and the highest temp for each day 
+                           if(forecastDay.main.temp_min < fiveDayForecast[date].temp_min) {
+                                fiveDayForecast[date].temp_min = forecastDay.main.temp_min;
+                           }
+                           if(forecastDay.main.temp_max > fiveDayForecast[date].temp_max) {
+                            fiveDayForecast[date].temp_max = forecastDay.main.temp_max;
+                            }
+                            
+                        }
+                        // if the 12pm data is available, display the 12pm day-time icon
+                        if (time === "12:00:00") {
+                            fiveDayForecast[date].icon = forecastDay.weather[0].icon;
+                        }
 
-            });
-                
-                
-        });   
+                    }
+
+                    //console.log(fiveDayForecast);
+
+                    for (let day in fiveDayForecast) {
+                        
+                        const luxonDate = DateTime.fromISO(day).toLocaleString(DateTime.DATE_SHORT);
+
+                        // Skips creating a forecast card for the current date
+                        if (currentDate === luxonDate) { continue }
+
+                        const forecastDay = fiveDayForecast[day];
+
+                        // Converts wind speed in m/s to km/h
+                        let wind = parseInt(forecastDay.wind)*3.6;
+
+                        let iconSrc = `http://openweathermap.org/img/wn/${forecastDay.icon}.png`;
+                        
+                        // 5-day forecast section displays: date, weather icon, temperature and humidity
+                        // Card component template for the 5 cards for 5-day-forecast
+                        const forecastCard = $(
+                            `
+                            <div class="card m-2">
+                            <h4 class="card-header">${luxonDate}</h4>
+                            <img style="width:70px" alt='${forecastDay.description}' src="${iconSrc}"/>
+                            <p>Min Temp: ${Math.round(forecastDay.temp_min)} ℃</p>
+                            <p>Max Temp: ${Math.round(forecastDay.temp_max)} ℃</p>
+                            <p>Wind: ${wind} KM/H</p>
+                            <p>Humidity: ${forecastDay.humidity}%</p>
+                            </div>
+                            `);
+                            
+                            $('.forecast-cards').append(forecastCard);
+                        
+                    }     
+                }); 
+
+            })
             
-                // Searched cities are added to the search history (localStorage).                
-                
-                     
-                
-    });
             
-            
+            // Searched cities are added to the search history (localStorage).                
             
             
             // When a user clicks on a city in the search history, current and future conditions for that city are shown again
@@ -126,12 +153,9 @@ $( document ).ready(function() {
             
             //     }
             //   }
+         
             
-    $('#search-input').val('Paris');
-    $('#search-button').click()
-            
-            
-});
+
         
         
         // event delegation because movie buttons are not created yet so we target the parent div buttons
@@ -164,7 +188,6 @@ $( document ).ready(function() {
         //     );
         
         //     $('#movies-info').prepend(movieInfo);
-        
-        //   });
-        // }
-        
+    })
+    
+});
